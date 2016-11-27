79a5245328393d862a2963e8d169463e6dce7108
Isaac Lewis <isaac.kaspar.lewis+github@gmail.com>
2016-11-27 15:44:52 +0000

    Various design changes

--- cellular.js
+++ cellular.js
@@ -7,3 +7,13 @@ var closePortfolio = function() {
     document.getElementById('portfolio-box').style.display = 'none';
     document.getElementById('about-box').style.display = 'block';
 }
+
+var showRecent = function() {
+    document.getElementById('old-projects').style.display = 'none';
+    document.getElementById('recent-projects').style.display = 'block';
+}
+
+var showOld = function() {
+    document.getElementById('recent-projects').style.display = 'none';
+    document.getElementById('old-projects').style.display = 'block';
+}
