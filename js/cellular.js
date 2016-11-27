ae263b59255ba8d6edd781b3f7f3b7ba5249f1d9
Isaac Lewis <isaac.kaspar.lewis+github@gmail.com>
2016-11-27 18:10:54 +0000

    Photoswipe files

---
+++ js/cellular.js
@@ -0,0 +1,9 @@
+var openPortfolio = function() {
+    document.getElementById('about-box').style.display = 'none';
+    document.getElementById('portfolio-box').style.display = 'block';
+}
+
+var closePortfolio = function() {
+    document.getElementById('portfolio-box').style.display = 'none';
+    document.getElementById('about-box').style.display = 'block';
+}
